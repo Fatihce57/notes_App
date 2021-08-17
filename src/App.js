5f37fdbe4a8ddae155048132782af3a00cd0f3fd
Fatih <fatihce57@gmail.com>
2021-08-18 01:43:45 +0200

    react-uuid installiered, onAddnote added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
 import SideBar from './SideBar';
 import Main from './Main';
+import { useState } from "react";
+import uuid from "react-uuid";    //npm install react-uuid//
 
 function App() {
+
+  const [notes, setNotes] = useState([]);
+
+  const onAddNote = () => {
+    const newNote = {
+      id: uuid(),
+      title: "Untitled Note",
+      body: "",
+      lastModified: Date.now(),
+    }
+    setNotes([newNote, ...notes]);
+  };
+
+
+
   return (
     <div className="App">
-     <SideBar/>
-     <Main/>
+      <SideBar notes={notes} onAddNote={onAddNote} />
+      <Main />
 
     </div>
   );

a2583de2d4860ef56c7ab654bbb8cd37bf99c409
Fatih <fatihce57@gmail.com>
2021-08-18 01:12:28 +0200

    sidebar and main html and css done

--- src/Main.js
+++ src/Main.js
@@ -2,8 +2,17 @@ import React from 'react'
 
 function Main() {
     return (
-        <div>
-            Main
+        <div className="app-main">
+            <div className="app-main-note-edit">
+                <input type="text" id="title" autoFocus />
+                <textarea id="body" placeholder="Write your note here..." />
+            </div>
+
+            <div className="app-main-note-preview">
+                <h1 className="preview-title">TITLE</h1>
+                <div className="markdown-preview">note preview</div>
+            </div>
+
         </div>
     )
 }
